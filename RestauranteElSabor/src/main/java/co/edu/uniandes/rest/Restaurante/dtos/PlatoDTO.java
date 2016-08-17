dab67fb9369bf7891920410540b6657ccd9bcf71
zl.castaneda10 <zl.castaneda10@uniandes.edu.co>
2016-08-16 22:26:37 -0500

    Definicion de la clase plato

---
+++ RestauranteElSabor/src/main/java/co/edu/uniandes/rest/Restaurante/dtos/PlatoDTO.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.rest.Restaurante.dtos;
+
+/**
+ *
+ * @author jdguz
+ */
+public class PlatoDTO 
+{
+    private Long id;
+    
+    private String nombre;
+    
+    private int precio;
+    
+    private String descripcion;
+    
+    
+    
+    public PlatoDTO()
+    {   }
+    
+    public PlatoDTO(Long pId, String pNombre, int pPrecio, String pDescripcion)
+    {
+        super();
+        this.id = pId;
+        this.nombre = pNombre;
+        this.precio = pPrecio;
+        this.descripcion = pDescripcion;
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() 
+    {
+        return precio;
+    }
+
+    public void setPrecio(int elPrecio) 
+    {
+        this.precio = elPrecio;
+    }
+
+    public String getDescripcion() 
+    {
+        return descripcion;
+    }
+
+    public void setDireccion(String direccion) 
+    {
+        this.descripcion = descripcion;
+    }
+        
+    /**
+     * Representacion JSON de un cliente
+     * @return La representacion JSON de un DTO cliente.
+     */
+    @Override
+    public String toString() 
+    {
+    	return "{ id : " + id +", nombre : \""+ nombre +"\", precio : \""+precio+"\", "
+                + "descripcion : \""+descripcion+"\" }" ;  
+    }
+    
+}
