05b69e6bc2242afd5870ebb4c3a6b6b5fd586c94
cc.novoa11 <cc.novoa11@ISIS2603-70.sis.virtual.uniandes.edu.co>
2016-08-30 10:36:19 -0500

    holi (modulo domiciliox2)

--- RestauranteElSabor/src/main/webapp/src/modules/domicilios/domicilios.mod.js
+++ RestauranteElSabor/src/main/webapp/src/modules/domicilios/domicilios.mod.js
@@ -4,4 +4,44 @@
  * and open the template in the editor.
  */
 
+(function (ng) {
+    var mod = ng.module("domiciliosModule", ["ngMessages"]);
+    mod.constant("domicilioContext", "api/domicilios");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/domicilios/';
+            $urlRouterProvider.otherwise("/domicilioList");
+     
+            $stateProvider.state('domicilioList', {
+                url: '/domicilios',
+                views: {
+                    'mainView': {
+                        controller: 'domiciliosCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'domicilios.list.html'
+                    }
+                }
+            }).state('domicilioCreate', {
+                url: '/cities/create',
+                views: {
+                    'mainView': {
+                        controller: 'domiciliosCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'domicilio.create.html'
+                    }
+                }
 
+            }).state('domicilioEdit', {
+                url: '/domicilios/:domicilioId',
+                param: {
+                    domicilioId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'domiciliossCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'domicilio.create.html'
+                    }
+                }
+            });
+        }]);
+})(window.angular);
