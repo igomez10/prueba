3560296c77a8872f5b419bdc761b64706d6ee788
zl.castaneda10 <zl.castaneda10@uniandes.edu.co>
2016-08-30 21:25:33 -0500

    Correcciones angular

--- RestauranteElSabor/src/main/webapp/src/modules/domicilios/domicilios.ctrl.js
+++ RestauranteElSabor/src/main/webapp/src/modules/domicilios/domicilios.ctrl.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 (function (ng) {
-    var mod = ng.module("domicilioModule");
+    var mod = ng.module("domiciliosModule");
 
         mod.controller("domicilioCtrl", ['$scope', '$state', '$stateParams', '$http', 'platoContext', function ($scope, $state, $stateParams, $http, context) {
 
