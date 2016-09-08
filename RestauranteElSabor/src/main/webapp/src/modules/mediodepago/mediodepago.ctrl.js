21e9ac9e1904a9dfd938ed316ec89191583e1b29
igomez10 <i.gomez10@uniandes.edu.co>
2016-09-07 22:03:27 -0500

    Revert "cambios iniciales medio de pago"
    This reverts commit 0482e6c4c39db503923bb0d85544b36d79e5ca04.

--- RestauranteElSabor/src/main/webapp/src/modules/mediodepago/mediodepago.ctrl.js
+++ RestauranteElSabor/src/main/webapp/src/modules/mediodepago/mediodepago.ctrl.js
@@ -1,113 +1,108 @@
-(function (ng) {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ (function (ng) {
+     var mod = ng.module("mediodepagoModule");
 
-    var mod = ng.module("medioDePagoModule");
+         mod.controller("mediodepagoCtrl", ['$scope', '$state', '$stateParams', '$http', 'mediodepagoContext', function ($scope, $state, $stateParams, $http, context) {
 
-    mod.controller("medioDePagoCtrl", ['$scope', '$state', '$stateParams', '$http', 'medioDePagoContext', function ($scope, $state, $stateParams, $http, context) {
+             // inicialmente el listado de ciudades está vacio
+             $scope.records = {};
+             // carga las ciudades
+             $http.get(context).then(function(response){
+                 $scope.records = response.data;
+             }, responseError);
 
-            // inicialmente el listado de medioDePago está vacio
-            $scope.records = {};
-            // carga los medios de pago
-            $http.get(context).then(function (response) {
-                $scope.records = response.data;
-            }, responseError);
+             // el controlador recibió un cityId ??
+             // revisa los parámetros (ver el :cityId en la definición de la ruta)
+             if ($stateParams.mediodepagoId !== null && $stateParams.mediodepagoId !== undefined) {
 
-            // el controlador recibió un medioDePagoId ??
-            // revisa los parámetros (ver el :medioDePagoId en la definición de la ruta)
-            if ($stateParams.medioDePagoId !== null && $stateParams.medioDePagoId !== undefined) {
-                // toma el id del parámetro
-                id = $stateParams.medioDePagoId;
-                // obtiene el dato del recurso REST
-                $http.get(context + "/" + id)
-                        .then(function (response) {
-                            // $http.get es una promesa
-                            // cuando llegue el dato, actualice currentRecord
-                            $scope.currentRecord = response.data;
-                        }, responseError);
+                 // toma el id del parámetro
+                 id = $stateParams.mediodepagoId;
+                 // obtiene el dato del recurso REST
+                 $http.get(context + "/" + id)
+                     .then(function (response) {
+                         // $http.get es una promesa
+                         // cuando llegue el dato, actualice currentRecord
+                         $scope.currentRecord = response.data;
+                     }, responseError);
 
-                // el controlador no recibió un medioDePagoId
-            } else
-            {
-                // el registro actual debe estar vacio
-                $scope.currentRecord = {
-                    id: undefined               /*Tipo Long. El valor se asigna en el backend*/,
-                    efectivo: undefined         /*Tipo boolean*/,
-                    tarjeta: ''                 /*Tipo String*/,
-                    numerosTarjeta: undefined   /*Tipo Long*/,
-                    fechaVencimiento: undefined /*Tipo Date*/,
-                    codigoSeguridad: undefined  /*Tipo Integer*/,
-                    franquicia: undefined       /*Tipo String*/
-                };
+             // el controlador no recibió un cityId
+             } else
+             {
+                 // el registro actual debe estar vacio
+                 $scope.currentRecord = {
+                     id: undefined /*Tipo Long. El valor se asigna en el backend*/,
+                     name: '' /*Tipo String*/
+                 };
 
-                $scope.alerts = [];
-            }
-
-            this.saveRecord = function (id, efectivo, tarjeta, numerosTarjeta, fechaVencimiento, codigoSeguridad, franquicia) {
-                currentRecord = $scope.currentRecord;
-                // si el id es null, es un registro nuevo, entonces lo crea
-                if (id == null) {
-                    // ejecuta POST en el recurso REST
-                    return $http.post(context, currentRecord)
-                            .then(function () {
-                                // $http.post es una promesa
-                                // cuando termine bien, cambie de estado
-                                $state.go('medioDePagoList');
-                            }, responseError);
-                    // si el id no es null, es un registro existente entonces lo actualiza
-                } else {
-
-                    // ejecuta PUT en el recurso REST
-                    return $http.put(context + "/" + currentRecord.id, currentRecord)
-                            .then(function () {
-                                // $http.put es una promesa
-                                // cuando termine bien, cambie de estado
-                                $state.go('medioDePagoList');
-                            }, responseError);
-                }
-                ;
-            };
-
-            this.deleteRecord = function (id) {
-                currentRecord = $scope.currentRecord;
-                $http.delete(context + "/" + currentRecord.id)
-                        .then(function () {
-                            // $http.put es una promesa
-                            // cuando termine bien, cambie de estado
-                            $state.go('medioDePagoList');
-                        }, responseError);
-
-            };
+                 $scope.alerts = [];
+             }
 
 
-            // -----------------------------------------------------------------
-            // Funciones para manejra los mensajes en la aplicación
-            //Alertas
-            this.closeAlert = function (index) {
-                $scope.alerts.splice(index, 1);
-            };
+             this.saveRecord = function (id) {
+                 currentRecord = $scope.currentRecord;
 
-            // Función showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
-            function showMessage(msg, type) {
-                var types = ["info", "danger", "warning", "success"];
-                if (types.some(function (rc) {
-                    return type === rc;
-                })) {
-                    $scope.alerts.push({type: type, msg: msg});
-                }
-            }
+                 // si el id es null, es un registro nuevo, entonces lo crea
+                 if (id == null) {
 
-            this.showError = function (msg) {
-                showMessage(msg, "danger");
-            };
+                     // ejecuta POST en el recurso REST
+                     return $http.post(context, currentRecord)
+                         .then(function () {
+                             // $http.post es una promesa
+                             // cuando termine bien, cambie de estado
+                             $state.go('mediodepagoList');
+                         }, responseError);
 
-            this.showSuccess = function (msg) {
-                showMessage(msg, "success");
-            };
+                 // si el id no es null, es un registro existente entonces lo actualiza
+                 } else {
 
-            var self = this;
-            function responseError(response) {
+                     // ejecuta PUT en el recurso REST
+                     return $http.put(context + "/" + currentRecord.id, currentRecord)
+                         .then(function () {
+                             // $http.put es una promesa
+                             // cuando termine bien, cambie de estado
+                             $state.go('mediodepagoList');
+                         }, responseError);
+                 };
+             };
 
-                self.showError(response.data);
-            }
-        }]);
 
-})(window.angular);
+
+             // -----------------------------------------------------------------
+             // Funciones para manejra los mensajes en la aplicación
+
+
+             //Alertas
+             this.closeAlert = function (index) {
+                 $scope.alerts.splice(index, 1);
+             };
+
+             // Función showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
+             function showMessage(msg, type) {
+                 var types = ["info", "danger", "warning", "success"];
+                 if (types.some(function (rc) {
+                     return type === rc;
+                 })) {
+                     $scope.alerts.push({type: type, msg: msg});
+                 }
+             }
+
+             this.showError = function (msg) {
+                 showMessage(msg, "danger");
+             };
+
+             this.showSuccess = function (msg) {
+                 showMessage(msg, "success");
+             };
+
+             var self = this;
+             function responseError(response) {
+
+                 self.showError(response.data);
+             }
+         }]);
+
+ })(window.angular);
--- RestauranteElSabor/src/main/webapp/src/modules/mediodepago/mediodepago.mod.js
+++ RestauranteElSabor/src/main/webapp/src/modules/mediodepago/mediodepago.mod.js
@@ -1,59 +1,52 @@
 /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ /*
   * To change this license header, choose License Headers in Project Properties.
   * To change this template file, choose Tools | Templates
   * and open the template in the editor.
   */
  (function(ng){
-     var mod = ng.module("medioDePagoModule",["ngMessages","ui.router"] );
-     mod.constant("medioDePagoContext", "api/medioDePago");
+     var mod = ng.module("mediodepagoModule",["ngMessages","ui.router"] );
+     mod.constant("mediodepagoContext", "api/mediosdepago");
         mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-             var basePath = 'src/modules/medioDePago/';
-             $urlRouterProvider.otherwise("/medioDePagoList");
+             var basePath = 'src/modules/mediodepago/';
+             $urlRouterProvider.otherwise("/mediodepagoList");
 
-             $stateProvider.state('medioDePagoList', {
-                 url: '/medioDePago',
+             $stateProvider.state('mediodepagoList', {
+                 url: '/mediosdepago',
                  views: {
                      'mainView': {
-                         controller: 'medioDePagoCtrl',
-                         controllerAs: 'ctrl',
-                         templateUrl: basePath + 'medioDePago.list.html'
+                         controller: 'mediodepagoCtrl',
+                         controllerAs: 'mediodepagoCtrl',
+                         templateUrl: basePath + 'mediodepago.list.html'
                      }
                  }
-             }).state('medioDePagoCreate', {
-                 url: '/medioDePago/create',
+             }).state('mediodepagoCreate', {
+                 url: '/mediosdepago/create',
                  views: {
                      'mainView': {
-                         controller: 'medioDePagoCtrl',
-                         controllerAs: 'ctrl',
-                         templateUrl: basePath + 'medioDePago.create.html'
+                         controller: 'mediodepagoCtrl',
+                         controllerAs: 'mediodepagoCtrl',
+                         templateUrl: basePath + 'mediodepago.create.html'
                      }
                  }
 
-             }).state('medioDePagoEdit', {
-                 url: '/medioDePago/:medioDePagoId',
+             }).state('mediodepagoEdit', {
+                 url: '/mediosdepago/:mediodepagoId',
                  param: {
-                     medioDePagoId: null
+                     mediodepagoId: null
                  },
                  views: {
                      'mainView': {
-                         controller: 'medioDePpagoCtrl',
-                         controllerAs: 'ctrl',
-                         templateUrl: basePath + 'medioDePago.create.html'
+                         controller: 'mediodepagoCtrl',
+                         controllerAs: 'mediodepagoCtrl',
+                         templateUrl: basePath + 'mediodepago.create.html'
                      }
                  }
-             }).state('medioDePagoDelete', {
-                url: '/medioDePago/:medioDePagoId',
-                param: {
-                    medioDePagoId: null
-                },
-                views:{
-                    'mainView': {
-                        controller: 'medioDePagoCtrl',
-                        controllerAs: 'ctrl',
-                        templateUrl: basePath + 'medioDePago.delete.html'
-                    }
-                }
-            });
-        }]);
-    
-})(window.angular);
+             });
+         }]);
+
+ })(window.angular);
